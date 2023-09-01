d1a7aebb4e554d345ec1c2a216aa3909838e9e65
HoooMaC <wawcam123@gmail.com>
2023-09-01 09:24:50 +0700

    restoring the window.h file

---
+++ Engine/src/Core/Window.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "Event/Event.h"
+
+#include "Core/Renderer/GraphicsContext.h"
+
+#include <functional>
+#include <string>
+
+#include <GLFW/glfw3.h>
+
+namespace Karem {
+
+	struct WindowProperty
+	{
+		std::string Title;
+		uint32_t Width, Height;
+
+		WindowProperty(
+			const std::string& title = "Default Window",
+			uint32_t width = 1280,
+			uint32_t height = 720
+		)
+			: Title(title), Width(width), Height(height) {}
+	};
+
+	class Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+
+		Window(const WindowProperty& property = WindowProperty());
+		~Window();
+
+		virtual void Init(const WindowProperty& property);
+		virtual void Shutdown();
+
+		virtual void OnUpdate();
+		virtual void SetVSync(bool enable);
+		virtual void SetEventCallbacks(EventCallbackFn callback);
+
+		virtual bool IsVSync();
+		virtual uint32_t GetWidth() const;
+		virtual uint32_t GetHeight() const;
+		virtual void* GetNativeWindow() const;
+	private:
+		GLFWwindow* m_Window;
+
+		struct WindowData
+		{
+			std::string Title;
+			uint32_t Width, Height;
+			bool VSync;
+
+			EventCallbackFn EventCallback;
+		};
+
+		WindowData m_Data;
+
+		std::unique_ptr<GraphicsContext> m_Context;
+	};
+
+}
