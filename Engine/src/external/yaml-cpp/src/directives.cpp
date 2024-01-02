76ce743e292ef316b251132c21af5001098db474
HoooMaC <wawcam123@gmail.com>
2024-01-02 14:37:17 +0700

    Setting the yaml library,

---
+++ Engine/src/external/yaml-cpp/src/directives.cpp
@@ -0,0 +1,18 @@
+#include <Core/Kareeem.h>
+#include "directives.h"
+
+namespace YAML {
+Directives::Directives() : version{true, 1, 2}, tags{} {}
+
+std::string Directives::TranslateTagHandle(
+    const std::string& handle) const {
+  auto it = tags.find(handle);
+  if (it == tags.end()) {
+    if (handle == "!!")
+      return "tag:yaml.org,2002:";
+    return handle;
+  }
+
+  return it->second;
+}
+}  // namespace YAML
